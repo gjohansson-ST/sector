594c6dc9686fe740a3d662310580326bcae3b985
Jonathan Petersson <jpetersson@redbridge.se>
2024-11-03 22:52:09 +0100

    Udate diagnostics

--- custom_components/sector/diagnostics.py
+++ custom_components/sector/diagnostics.py
@@ -9,18 +9,14 @@ from .coordinator import SectorDataUpdateCoordinator
 
 
 async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, config_entry: ConfigEntry
+    hass: HomeAssistant, entry: ConfigEntry
 ):
     """Return diagnostics for a config entry."""
-    coordinator: SectorDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator: SectorDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+
     diagnostics_data = {
-        "config_entry": {
-            "title": config_entry.title,
-            "data": {
-                "email": config_entry.data.get("email"),
-                "panel_id": config_entry.data.get("panel_id"),
-            },
-        },
         "data": coordinator.data,
+        "entry": entry.as_dict(),
     }
+
     return diagnostics_data
