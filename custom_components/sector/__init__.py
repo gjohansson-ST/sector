fb489bd0b2ee6330679aed5d022d1b66837def15
Jonathan Petersson <jpetersson@redbridge.se>
2024-11-08 13:47:12 +0100

    Remove unessary stuff

--- custom_components/sector/__init__.py
+++ custom_components/sector/__init__.py
@@ -14,7 +14,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: SectorAlarmConfigEntry) 
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
 
-    # Register the listener for configuration updates
     entry.async_on_unload(entry.add_update_listener(async_update_listener))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
