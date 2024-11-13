fc4b309ac240f815c56adfa3fe9bc8a422c1f09a
G Johansson <goran.johansson@shiftit.se>
2024-11-13 21:03:17 +0100

    Add model to sector

---
+++ custom_components/sector/model.py
@@ -0,0 +1,42 @@
+"""Models for Sector."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class SectorData:
+    """Dataclass for all data."""
+    alarm: PanelStatus
+    devices: Devices
+    locks: Locks
+
+@dataclass
+class Locks:
+    """Dataclass Locks."""
+
+    name: str
+    serial_no: str
+    lock_status: str
+    low_battery: str # Needs to be checked
+    model: str = "Smart Lock"
+
+@dataclass
+class Devices:
+    """Dataclass for devices."""
+
+    name: str
+    serial_no: str
+    device_status: str # Needs to be checked
+    model: str
+    type: str
+
+
+@dataclass
+class PanelStatus:
+    """Dataclass for Alarm Panel."""
+
+    alarm_state: int
+    is_online: bool
+
